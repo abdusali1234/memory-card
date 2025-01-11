9c2d95619c940744bb36c2331cd1a27e482ab509
abdusali1234 <abdusali1234@gmail.com>
2025-01-11 17:01:15 +0000

    added fuctions to kep track of scores and to shuffle cards. Issue with modal not showing and not saying ggame over if same icons is clicked twice (but works when clicked thrice)

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,133 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import Card from "./components/Card";
+import Modal from "./components/Modal";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // Some cool pokemon from when I was a wee lad
+  const pokemons = [
+    {
+      id: 0,
+      name: "charizard",
+    },
+    {
+      id: 1,
+      name: "lucario",
+    },
+    {
+      id: 2,
+      name: "piplup",
+    },
+    {
+      id: 3,
+      name: "pikachu",
+    },
+    {
+      id: 4,
+      name: "eevee",
+    },
+    {
+      id: 5,
+      name: "squirtle",
+    },
+    {
+      id: 6,
+      name: "psyduck",
+    },
+    {
+      id: 7,
+      name: "diglett",
+    },
+    {
+      id: 8,
+      name: "mew",
+    },
+  ];
+
+  const [pokemonData, setPokemonData] = useState([]);
+
+  function shuffle(arr) {
+    let shuffled = arr
+      .map((val) => ({ val, sort: Math.random() }))
+      .sort((a, b) => a.sort - b.sort)
+      .map(({ val }) => val);
+    return shuffled;
+  }
+
+  async function getPokemonData() {
+    try {
+      const fetchedResults = await Promise.all(
+        pokemons.map(async (pokemon) => {
+          console.log(pokemon);
+          const response = await fetch(
+            `https://pokeapi.co/api/v2/pokemon/${pokemon.name}`
+          );
+          const data = await response.json();
+          return {
+            ...pokemon,
+            imageUrl: data.sprites.other.dream_world.front_default,
+            name: pokemon.name[0].toUpperCase() + pokemon.name.slice(1),
+          };
+        })
+      );
+
+      setPokemonData(() => shuffle(fetchedResults));
+    } catch (err) {
+      console.log("Error fetching from Pokemon API: ", err);
+    }
+  }
+
+  useEffect(() => {
+    getPokemonData();
+  }, []);
+
+  const [selectedPokemonIds, setSelectedPokemonIds] = useState([]);
+  const [currScore, setCurrScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
+  const [modalDialogMessage, setModalDialogMessage] = useState("");
+
+  function checkGameStatus(pokemonId) {
+    if (selectedPokemonIds.includes(pokemonId)) {
+      console.log(selectedPokemonIds);
+      setModalDialogMessage(() => "Game Over!");
+      console.log(modalDialogMessage);
+    } else {
+      setSelectedPokemonIds((prev) => [...prev, pokemonId]);
+      console.log(selectedPokemonIds);
+      setCurrScore((currScore) => currScore + 1);
+      setPokemonData(() => shuffle(pokemonData));
+      if (currScore == 8) {
+        setModalDialogMessage(() => "You won!");
+      }
+    }
+  }
+
+  function resetGame() {
+    if (currScore > bestScore) {
+      setBestScore(() => currScore);
+    }
+    setCurrScore(() => 0);
+    setSelectedPokemonIds(() => []);
+    setModalDialogMessage(() => "");
+    setPokemonData(() => shuffle(pokemonData));
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      {modalDialogMessage && (
+        <Modal message={modalDialogMessage} handleChange={resetGame}></Modal>
+      )}
+      <div className="pokemon-card-grid">
+        {pokemonData.map((pokemon) => (
+          <Card
+            {...pokemon}
+            key={pokemon.id}
+            handleChange={() => checkGameStatus(pokemon.id)}
+          ></Card>
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,6 @@
 export default function Card({ name, id, imageUrl, handleChange }) {
   return (
-    <div className="pokemonCard" key={id} onClick={handleChange}>
+    <div className="pokemon-card" key={id} onClick={handleChange}>
       <img src={imageUrl} alt={name} />
       <span>{name}</span>
     </div>
